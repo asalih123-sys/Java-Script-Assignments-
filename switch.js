88c862e306c79d8a30035d394d85a0dc33324d7c
Amer Mohamed Salih <ame2164160@maricopa.edu>
2025-10-23 14:28:04 -0700

    Add files via upload

--- switch.js
+++ switch.js
@@ -8,19 +8,23 @@ const prompt = require('prompt-sync')();
         let price; 
     switch(coffeeSize)
     {
-        case "Small" && "small":
+        case "Small": 
+        case "small":
             price = 2.50;
             break;
 
-        case "Medium" || "medium":
+        case "Medium":
+        case "medium":
             price = 3.50;
             break;
 
-        case "Large" || "large":
+        case "Large":
+        case "large":
             price = 4.25;
             break;
 
-        case "x-large" || "X-Large":
+        case "x-large":
+        case "X-Large":
             price = 5.00;
             break;
 
@@ -44,7 +48,7 @@ console.log("The Price of your coffee is: $" + price + "\n");
                 case  "January":
                 case  "february":
                 case  "February":
-                    console.log("The Season" + month + " belongs to is Winter! \n");
+                    console.log("The Season " + month + " belongs to is Winter! \n");
                     break;
                 
                 case "march":
@@ -62,7 +66,7 @@ console.log("The Price of your coffee is: $" + price + "\n");
                 case "july":
                 case "August": 
                 case "august":
-                    console.log("The Season" + month + " belongs to is Summer! \n");
+                    console.log("The Season " + month + " belongs to is Summer! \n");
                     break;
 
                 case "september":
@@ -71,7 +75,7 @@ console.log("The Price of your coffee is: $" + price + "\n");
                 case "October": 
                 case "november":
                 case "November":
-                    console.log("The Season" + month + " belongs to is Fall! \n");
+                    console.log("The Season " + month + " belongs to is Fall! \n");
                     break;
                 
                 default:
@@ -81,4 +85,52 @@ console.log("The Price of your coffee is: $" + price + "\n");
 
         //=======================================================================================
 
-        // Question 3 - 
+        // Question 3 - Movie Rating Guide
+            console.log("Question 3 - Movie Rating Guide");
+            let mName = prompt("Enter a movie name: ");
+            let mRating = prompt("Enter a movie rating: ");
+
+                switch(mRating)
+                {
+                    case "G":
+                    console.log("G (General Audiences): Suitable for all ages. Contains nothing that would offend parents for viewing by children. \n");
+                    break;
+
+                    case "PG":
+                    console.log("PG (Parental Guidance Suggested): Some material may not be suitable for children. Parental guidance is recommended. \n");
+                    break;
+
+                    case "PG-13":
+                        console.log("PG-13 (Parents Strongly Cautioned): Some material may be inappropriate for children under 13. \n");
+                    break;
+
+                    case "R":
+                    console.log("R (Restricted): Under 17 requires accompanying parent or adult guardian. \n");
+                    break;
+
+                    case "NC-17":
+                        console.log("NC-17 (No One 17 and Under Admitted): Clearly adult films. No one 17 and under is admitted, even with a parent or guardian. \n");
+                    break;
+
+                    default:
+                        console.log("Invalid Answer \n");
+                        break;
+                }
+//==================================================================================================================================
+    
+// Question 4 - Programming Language
+    console.log("Question 4 - Programming Language");
+    let pLanguage = prompt("Enter a programming language file extension: ");
+        
+            switch(pLanguage)
+            {
+                
+
+
+
+
+
+
+
+
+            }
